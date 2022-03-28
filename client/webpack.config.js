9c22f6827d826d1a6c2a167e65b4d0681ec26b04
Matthew Williams <mattwilliamsdev@gmail.com>
2022-03-28 15:17:01 +0100

    Got my service worker working

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -26,9 +26,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "text-editor",
+        name: "Just Another Text Editor",
         short_name: "JATE",
-        description: "Just Another Text Editor!",
+        description: "Take notes with JavaScript Syntax Highlighting!",
         background_color: "#225ca3",
         theme_color: "#225ca3",
         start_url: "/",
